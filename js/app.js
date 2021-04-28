0480e78f7713131db563424756c19c87821cb783
William Rojas <wrojas@glingeriesas.com>
2021-04-28 00:36:12 -0500

    Some changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 
 // se define la lista de productos disponibles para comprar con su respectivo precio
-const productos = [
-    { nombre: 'router', descripcion: 'Router inalámbrico marca Dlink', precio: 150000 },
-    { nombre: 'antena', descripcion: 'LiteBeam 5Ghz', precio: 230000 },
-    { nombre: 'internet', descripcion: 'Dedicado 1:1', precio: 80000 },
-    { nombre: 'amplificador', descripcion: 'Amplicifador de rango 5Ghz', precio: 200000 }
+var productos = [
+    { idProducto: 1, nombre: 'router', descripcion: 'Router inalámbrico marca Dlink', precio: 150000 },
+    { idProducto: 2, nombre: 'antena', descripcion: 'LiteBeam 5Ghz', precio: 230000 },
+    { idProducto: 3, nombre: 'internet', descripcion: 'Dedicado 1:1', precio: 80000 },
+    { idProducto: 4, nombre: 'amplificador', descripcion: 'Amplicifador de rango 5Ghz', precio: 200000 }
 ];
+
+
+var bdClientes = [];
 
 console.log('listado de productos');
 console.table(productos);
@@ -23,8 +26,10 @@ class articulo {
     }
 }
 
+
 class clientes {
-    constructor(nombreCliente, apellidoCliente, telefono, email) {
+    constructor(idCliente, nombreCliente, apellidoCliente, telefono, email) {
+        this.idCliente=idCliente,
         this.nombreCliente = nombreCliente,
             this.apellidoCliente = apellidoCliente,
             this.telefono = telefono,
@@ -64,7 +69,7 @@ let factura = {
     }
 };
 
-/* factura.cliente.nombreCliente = prompt('Escribe tu nombre');
+factura.cliente.nombreCliente = prompt('Escribe tu nombre');
 factura.cliente.apellidoCliente = prompt('Escribe tu apellido');
 factura.cliente.telefono = prompt('Escribe tu numero de teléfono');
 factura.cliente.email = prompt('Escribe tu email');
@@ -86,27 +91,54 @@ factura.fecha = hoy.toLocaleDateString();
 
 factura.calculoFactura();
 console.table(factura.producto)
-console.table(factura.total)*/
+console.table(factura.total)
 
-function showProductTable(tabla) {
-    var table = document.getElementById("productTable");
+function showProductTable(tabla, targetId, modalGroup) {
+    var table = document.getElementById(targetId);
+    table.tBodies[0].innerHTML='';
     for (let i = 0; i < tabla.length; i++) {
-
         let row = table.tBodies[0].insertRow(i);
         let j = 0;
-        let cell1 = row.insertCell(j);
-        cell1.innerHTML = i + 1;
-        j++;
         for (const property in tabla[i]) {
             let cell1 = row.insertCell(j);
             cell1.innerHTML = tabla[i][property];
             j++;
         }
+        let cell1 = row.insertCell(j);
+        
+        let miBoton = document.createElement("button");
+        miBoton.classList.add("btn");
+        miBoton.classList.add("btn-outline-success");
+        miBoton.classList.add("me-1");
+        miBoton.setAttribute('data-bs-toggle', 'modal');
+        miBoton.setAttribute('data-bs-target', `#edit${modalGroup}Modal`);
+        miBoton.title = 'Editar';
+        let miSpan = document.createElement("span");
+        miSpan.classList.add("bi");
+        miSpan.classList.add("bi-pen");
+        miBoton.appendChild(miSpan);
+
+        let miBoton2 = document.createElement("button");
+        miBoton2.classList.add("btn");
+        miBoton2.classList.add("btn-outline-danger");
+        miBoton2.classList.add("me-1");
+        miBoton2.setAttribute('data-bs-toggle', 'modal');
+        miBoton2.setAttribute('data-bs-target', `#del${modalGroup}Modal`);
+        miBoton2.title = 'Borrar';
+        let miSpan2 = document.createElement("span");
+        miSpan2.classList.add("bi");
+        miSpan2.classList.add("bi-pen");
+        miBoton2.appendChild(miSpan2);
+
+        cell1.appendChild(miBoton) ;
+        cell1.appendChild(miBoton2) ;
+
     }
 
 }
 
-showProductTable(productos);
+
+showProductTable(productos, "productTable", "product");
 
 function ordenarproductos() {
     productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
