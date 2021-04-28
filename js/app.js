59ba5d1d458293fb6357ca453a2c20b4406122ac
William Rojas <wrojas@glingeriesas.com>
2021-04-28 03:29:47 -0500

    Some changes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 
 // se define la lista de productos disponibles para comprar con su respectivo precio
 var productos = [
-    { idProducto: 1, nombre: 'router', descripcion: 'Router inalámbrico marca Dlink', precio: 150000 },
-    { idProducto: 2, nombre: 'antena', descripcion: 'LiteBeam 5Ghz', precio: 230000 },
-    { idProducto: 3, nombre: 'internet', descripcion: 'Dedicado 1:1', precio: 80000 },
-    { idProducto: 4, nombre: 'amplificador', descripcion: 'Amplicifador de rango 5Ghz', precio: 200000 }
+    { id: 1, nombre: 'router', descripcion: 'Router inalámbrico marca Dlink', precio: 150000 },
+    { id: 2, nombre: 'antena', descripcion: 'LiteBeam 5Ghz', precio: 230000 },
+    { id: 3, nombre: 'internet', descripcion: 'Dedicado 1:1', precio: 80000 },
+    { id: 4, nombre: 'amplificador', descripcion: 'Amplicifador de rango 5Ghz', precio: 200000 }
 ];
 
 
@@ -28,9 +28,9 @@ class articulo {
 
 
 class clientes {
-    constructor(idCliente, nombreCliente, apellidoCliente, telefono, email) {
-        this.idCliente=idCliente,
-        this.nombreCliente = nombreCliente,
+    constructor(id, nombreCliente, apellidoCliente, telefono, email) {
+        this.id = id,
+            this.nombreCliente = nombreCliente,
             this.apellidoCliente = apellidoCliente,
             this.telefono = telefono,
             this.email = email
@@ -69,33 +69,33 @@ let factura = {
     }
 };
 
-factura.cliente.nombreCliente = prompt('Escribe tu nombre');
+/* factura.cliente.nombreCliente = prompt('Escribe tu nombre');
 factura.cliente.apellidoCliente = prompt('Escribe tu apellido');
 factura.cliente.telefono = prompt('Escribe tu numero de teléfono');
-factura.cliente.email = prompt('Escribe tu email');
+factura.cliente.email = prompt('Escribe tu email'); */
 
 let newItem = true;
 
-while (newItem != false) {
+/* while (newItem != false) {
     let concepto = prompt('¿Qué quieres comprar?');
     let cantidad = prompt('Cantidad requerida');
     factura.producto.push(new articulo(factura.producto.length + 1, concepto, cantidad))
     factura.producto[factura.producto.length - 1].valorUnitario();
     newItem = confirm('Deseas agregar un nuevo producto?');
-} 
+}  */
 
 factura.numeroFactura = '001'; // dependerá de un ente externo
 const tiempoTranscurrido = Date.now();
 const hoy = new Date(tiempoTranscurrido);
 factura.fecha = hoy.toLocaleDateString();
 
-factura.calculoFactura();
+//factura.calculoFactura();
 console.table(factura.producto)
 console.table(factura.total)
 
 function showProductTable(tabla, targetId, modalGroup) {
     var table = document.getElementById(targetId);
-    table.tBodies[0].innerHTML='';
+    table.tBodies[0].innerHTML = '';
     for (let i = 0; i < tabla.length; i++) {
         let row = table.tBodies[0].insertRow(i);
         let j = 0;
@@ -105,13 +105,15 @@ function showProductTable(tabla, targetId, modalGroup) {
             j++;
         }
         let cell1 = row.insertCell(j);
-        
+
         let miBoton = document.createElement("button");
         miBoton.classList.add("btn");
         miBoton.classList.add("btn-outline-success");
         miBoton.classList.add("me-1");
         miBoton.setAttribute('data-bs-toggle', 'modal');
+        miBoton.setAttribute('data-itemId', tabla[i].id);
         miBoton.setAttribute('data-bs-target', `#edit${modalGroup}Modal`);
+        miBoton.id = 'btnEditar' + tabla[i].id;
         miBoton.title = 'Editar';
         let miSpan = document.createElement("span");
         miSpan.classList.add("bi");
@@ -123,22 +125,24 @@ function showProductTable(tabla, targetId, modalGroup) {
         miBoton2.classList.add("btn-outline-danger");
         miBoton2.classList.add("me-1");
         miBoton2.setAttribute('data-bs-toggle', 'modal');
+        miBoton.setAttribute('data-itemId', tabla[i].id);
         miBoton2.setAttribute('data-bs-target', `#del${modalGroup}Modal`);
+        miBoton2.id = 'btnBorrar' + tabla[i].id;
         miBoton2.title = 'Borrar';
         let miSpan2 = document.createElement("span");
         miSpan2.classList.add("bi");
-        miSpan2.classList.add("bi-pen");
+        miSpan2.classList.add("bi-eraser");
         miBoton2.appendChild(miSpan2);
 
-        cell1.appendChild(miBoton) ;
-        cell1.appendChild(miBoton2) ;
+        cell1.appendChild(miBoton);
+        cell1.appendChild(miBoton2);
 
     }
 
 }
 
 
-showProductTable(productos, "productTable", "product");
+showProductTable(productos, "productTable", "Product");
 
 function ordenarproductos() {
     productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
@@ -146,11 +150,34 @@ function ordenarproductos() {
 }
 
 function ocultaph() {
-    Array.from(document.getElementsByClassName('ph')).map(container => container.className='ph inactivo');
+    Array.from(document.getElementsByClassName('ph')).map(container => container.className = 'ph inactivo');
 }
 
 function desactivaBoton() {
-    Array.from(document.getElementsByClassName('nav-link')).map(navButton => navButton.className='nav-link text-white');
+    Array.from(document.getElementsByClassName('nav-link')).map(navButton => navButton.className = 'nav-link text-white');
+}
+
+function guardaProducto(e) {
+    let nombre = document.getElementById('ctrlAddNombreProd');
+    let desc = document.getElementById('ctrlAddDescProd');
+    let precio = document.getElementById('ctrlAddPrecioProd');
+
+    if(nombre.value == '' || desc.value == '' || precio.value == ''){
+        $('#addProductModal').modal('hide');
+        return;
+    }
+
+    productos.push({
+        id: productos.length + 1,
+        nombre: nombre.value.toLowerCase(),
+        descripcion: desc.value,
+        precio: precio.value
+    });
+    $('#addProductModal').modal('hide');
+    nombre.value = "";
+    desc.value = "";
+    precio.value = "";
+    showProductTable(productos, "productTable", "Product");
 }
 
 window.onload = function () {
@@ -160,14 +187,14 @@ window.onload = function () {
         ocultaph();
         desactivaBoton();
         document.getElementById(button.dataset["phtarget"]).className = "ph";
-        button.className= 'nav-link text-white active';
+        button.className = 'nav-link text-white active';
     });
 
 }
 
-$(document).ready(function(){
-	// Activate tooltip
-	$('[data-toggle="tooltip"]').tooltip();
+$(document).ready(function () {
+    // Activate tooltip
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
 
